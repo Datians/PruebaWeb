8d81ec8cb934751a8807b385916ed654a42ebd8f
Datians <Dacuadrado123@gmail.com>
2024-12-12 13:18:15 -0500

    segundo commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
     const date = new Date();
     return {
       message: 'Hola, la fecha y hora de hoy es:',
-      
+      mensage: 'Hola, buena tarde',
       date: date.toLocaleString(),
       
     };
